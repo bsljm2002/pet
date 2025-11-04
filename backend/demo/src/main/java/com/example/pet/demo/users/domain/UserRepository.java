67efd710f695759a49ca5987ea389ed7fc6ce6e4
bsljm2002 <bsljm2002@naver.com>
2025-11-04 13:43:48 +0900

    회원가입 구현 중

--- backend/demo/src/main/java/com/example/pet/demo/users/domain/UserRepository.java
+++ backend/demo/src/main/java/com/example/pet/demo/users/domain/UserRepository.java
@@ -1,8 +1,7 @@
 package com.example.pet.demo.users.domain;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public class UserRepository {
-
+public interface UserRepository extends JpaRepository<User, Long>{
+    boolean existsByEmail(String email);
 }
