6bf6ea4011427877fc7c715354d9533a12e9512d
bsljm2002 <bsljm2002@naver.com>
2025-11-12 21:46:14 +0900

    사용자 조회 구현 중

--- backend/demo/src/main/java/com/example/pet/demo/users/infra/jpa/UserJpaRepository.java
+++ backend/demo/src/main/java/com/example/pet/demo/users/infra/jpa/UserJpaRepository.java
@@ -1,5 +1,6 @@
 package com.example.pet.demo.users.infra.jpa;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.example.pet.demo.users.domain.User;
+import com.example.pet.demo.users.domain.User.UserType;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
@@ -16,4 +18,7 @@ public interface UserJpaRepository extends JpaRepository<User, Long> {
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("update User u set u.lastLoginAt = CURRENT_TIMESTAMP where u.id = :id")
     int touchLastLogin(Long id);
+
+    List<User> findByUserType(UserType type);
+
 }
